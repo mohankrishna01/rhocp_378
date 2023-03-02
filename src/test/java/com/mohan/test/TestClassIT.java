f9ba1e4e493681352828c95456558456772e0190
Mohan Krishna  R <60535363+mohankrishna01@users.noreply.github.com>
2023-03-02 19:01:38 +0530

    Add files via upload

---
+++ src/test/java/com/mohan/test/TestClassIT.java
@@ -0,0 +1,8 @@
+package com.mohan.test;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class TestClassIT extends TestClassTest {
+    // Execute the same tests but in packaged mode.
+}
